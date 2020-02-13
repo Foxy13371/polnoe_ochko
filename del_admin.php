dd65571ec1033e08559e230c00f662d320250a86
Foxy13371 <59532707+Foxy13371@users.noreply.github.com>
2020-02-13 16:26:16 +0300

    Add files via upload

---
+++ del_admin.php
@@ -0,0 +1,10 @@
+<?php
+$open = mysqli_connect('localhost', 'root', '', 'airlines', '3306');
+if (!$open){
+    echo 'Код ошибки :'. mysqli_connect_errno();
+    echo 'Текст ошибки:'. mysqli_connect_error();
+    exit;
+}
+$res ="delete * from flying where id = '5'";
+header ('Location: airadmin.php');
+?>
