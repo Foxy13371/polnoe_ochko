3058ec34bb24e51549ba10ab8439cabd15bd4904
Foxy13371 <59532707+Foxy13371@users.noreply.github.com>
2020-03-12 22:18:34 +0300

    Delete admin.js

--- admin.js
+++
@@ -1,8 +0,0 @@
-pass = 'admin';
-ans = prompt ("Введите пароль:");
-if (pass===ans){
-    alert ("Склоняяяюсь перед вашей волей");
-} else {
-    alert("Нет доступа к базе данных!");
-    window.location.href = 'airbase.php';
-}
